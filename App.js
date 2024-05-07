575a947fd37550cadbd1e42e217114767bba2fad
Mike Chan <micemaze@gmail.com>
2024-05-07 22:06:15 +0800

    modified:   App.js 	new file:   assets/data/card_database.csv 	new file:   assets/data/card_database.json 	new file:   assets/data/card_database.xlsm 	new file:   assets/images/background-image.png 	new file:   assets/images/background.jpg 	new file:   assets/images/dice-80.png 	new file:   assets/images/emoji1.png 	new file:   assets/images/emoji2.png 	new file:   assets/images/emoji3.png 	new file:   assets/images/emoji4.png 	new file:   assets/images/emoji5.png 	new file:   assets/images/emoji6.png 	new file:   assets/images/foreign-language-sound-64.png 	new file:   assets/images/next-64.png 	new file:   assets/images/no-audio-50.png 	new file:   assets/images/previous-64.png 	new file:   assets/images/speaker-50.png 	new file:   assets/images/speaker-64.png 	new file:   assets/images/translation-50.png 	new file:   assets/images/voice-64.png 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,20 +1,169 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, ImageBackground,useWindowDimensions } from 'react-native';
+//import FontAwesomeIcon from "@expo/vector-icons/FontAwesome";
+import card_database from "./assets/data/card_database.json";
+import * as Speech from 'expo-speech';
+import bgImg from "./assets/images/background-image.png";
+
+//import Tts from 'react-native-tts';
 
 export default function App() {
+  // const card_database = "./assets/data/card_database.csv";
+  const nextButtonImg = "./assets/images/next-64.png";
+  const prevButtonImg = "./assets/images/previous-64.png";
+  const speakerImg = "./assets/images/foreign-language-sound-64.png";
+  const translateImg = "./assets/images/translation-50.png";
+  const noPronounImg = "./assets/images/no-audio-50.png";
+  const showPronounImg = "./assets/images/speaker-50.png";
+  const randomImg = "./assets/images/dice-80.png";
+  const {winHeight, winWidth} = useWindowDimensions();
+
+  const [itemSeq, setItemSeq] = useState(0);
+  const [showTranslate, setShowTranslate] = useState(1);
+  const [showPronoun, setShowPronoun] = useState(1);
+
+  //Tts.setDefaultLanguage('en-IE');
+  // Tts.addEventListener('tts-start', event => console.log('start', event));
+  // Tts.addEventListener('tts-finish', event => console.log('finish', event));
+  // Tts.addEventListener('tts-cancel', event => console.log('cancel', event));
+function readWord( theWord ) {
+  //Tts.stop();
+  //Tts.speak(theWord)
+    Speech.speak(theWord, {language:'ja-JP'});
+}
+
+  function nextCard() {
+    if (itemSeq < card_database.length-1) {
+      setItemSeq(itemSeq+1);
+    }
+    console.log("items ", itemSeq,card_database.length )
+    console.log("width - height", winWidth, winHeight, bgImg)
+  }
+
+  function prevCard() {
+    if (itemSeq >= 1) {
+      setItemSeq(itemSeq-1);
+    }
+    console.log("items ", itemSeq,card_database.length )
+
+  }
+  function randomCard() {
+    setItemSeq( Math.floor(Math.random() * (card_database.length-1)) );
+  }
+  function showOnOff() {
+    setShowTranslate(showTranslate===0?1:0);
+  }
+
+  function pronounOnOff() {
+    setShowPronoun(showPronoun===0?1:0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground style={styles.img} source={bgImg} resizeMode='cover' />     
+      <View style={styles.modalView}>
+        <Text style={styles.title}>{card_database[itemSeq].Title}</Text>
+        <Text style={styles.subtitle}>{card_database[itemSeq].Subtitle === "" ? "": "(" + card_database[itemSeq].Subtitle + ")"}</Text>
+        <Text style={styles.blank}></Text>
+        <Pressable style={styles.barView} 
+              onPress={() => {Speech.speak((card_database[itemSeq].Subtitle === "" ? card_database[itemSeq].Title:card_database[itemSeq].Subtitle), {language:'ja-JP'});}}>
+            <Image style={[styles.speakerImg,{opacity:showPronoun}]} source={require(speakerImg) }/>
+            <Text style={[styles.pronoun,{opacity:showPronoun}]}>  {card_database[itemSeq].Pronoun}</Text>
+        </Pressable>
+        <Text style={styles.blank}></Text>
+        <Text style={[styles.description,{opacity:showTranslate}]}>{card_database[itemSeq].Description}</Text>
+        <StatusBar style="auto"/>
+      </View>
+
+      <View style={styles.barView}>
+        <Pressable onPress={showOnOff}>
+          <Image style={[styles.translateImgSize,{opacity:(showTranslate===1?1:0.1)}]} source={require(translateImg) }/>
+        </Pressable>
+        <Text>    </Text>
+        <Pressable onPress={pronounOnOff}>
+          <Image style={[styles.noPronounImgSize,{opacity:(showPronoun===1?1:0.1)}]} source={(showPronoun===1?require(showPronounImg):require(noPronounImg))}/>
+        </Pressable>
+        <Text>    </Text>
+        <Pressable onPress={randomCard}>
+          <Image style={styles.noPronounImgSize} source={require(randomImg)}/>
+        </Pressable>
+        <Text>            </Text>
+        <Pressable onPress={prevCard}>
+          <Image style={[styles.buttonImg, {opacity:(itemSeq===0?0.1:1)}]} source={require(prevButtonImg) }/>
+        </Pressable>
+        <Text>    </Text>
+        <Pressable onPress={nextCard}>
+          <Image style={[styles.buttonImg,{opacity:(itemSeq+1 === card_database.length ?0.1:1)}]} source={require(nextButtonImg)}/>
+        </Pressable>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    top:'10%',
     alignItems: 'center',
+    marginTop:30,
     justifyContent: 'center',
   },
+  modalView: {
+    margin: 20,
+    width:"95%",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    backgroundColor: 'white',
+  },
+  barView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonImg:{
+    width: 32,
+    height: 32,
+  },
+  translateImgSize:{
+    width: 20,
+    height: 20,
+  },
+  noPronounImgSize:{
+    width: 20,
+    height: 20,
+  },
+  speakerImg:{
+    width: 16,
+    height: 16,
+  },
+  title: {
+    fontSize: 40,
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+  },
+  pronoun: {
+    fontSize: 20,
+    fontStyle: 'italic',
+  },
+  description: {
+    fontSize: 20,
+  },
+  blank: {
+    fontSize: 10,
+  },
+  img: { 
+    justifyContent: 'center', 
+  }, 
 });
